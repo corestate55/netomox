8b93ec8e059713d597344efbe6f459b25dedee4d
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2022-07-30 12:08:26 +0900

    Fix file add

---
+++ lib/netomox/dsl/node_attr_mddo_redistribute.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'netomox/const'
+
+module Netomox
+  module DSL
+    # Redistribute config for MDDO ospf-area node attribute
+    class MddoOspfAreaRedistribute
+      # @!attribute [rw] protocol
+      #   @return [String] TODO: enum{static, connected}
+      # @!attribute [rw] metric_type
+      #   @return [Integer] TODO: enum{1, 2} (OE1, OE2)
+      attr_accessor :protocol, :metric_type
+
+      # @param [String] protocol
+      # @param [Integer] metric_type
+      def initialize(protocol: 'connected', metric_type: 2)
+        @protocol = protocol
+        @metric_type = metric_type
+      end
+
+      # Convert to RFC8345 topology data
+      # @return [Hash]
+      def topo_data
+        {
+          'protocol' => @protocol,
+          'metric-type' => @metric_type
+        }
+      end
+    end
+  end
+end
---
+++ lib/netomox/dsl/node_attr_mddo_static_route.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'netomox/const'
+
+module Netomox
+  module DSL
+    # Static routes for MDDO L3 node attribute
+    class MddoStaticRoute
+      # @!attribute [rw] prefix
+      #   @return [String] IP address/prefix-length
+      # @!attribute [rw] next_hop
+      #   @return [String] IP address
+      # @!attribute [rw] interface
+      #   @return [String] interface (device) name
+      # @!attribute [rw] metric
+      #   @return [Integer]
+      # @!attribute [rw] preference
+      #   @return [Integer]
+      # @!attribute [rw] description
+      #   @return [String]
+      attr_accessor :prefix, :next_hop, :interface, :metric, :preference, :description
+
+      # rubocop:disable Metrics/ParameterLists
+      # @param [String] prefix
+      # @param [String] next_hop
+      # @param [String] interface
+      # @param [Integer] metric
+      # @param [Integer] preference
+      # @param [String] description
+      def initialize(prefix: '', next_hop: '', interface: '', metric: 10, preference: 1, description: '')
+        @prefix = prefix
+        @next_hop = next_hop
+        @interface = interface
+        @metric = metric
+        @preference = preference
+        @description = description
+      end
+      # rubocop:enable Metrics/ParameterLists
+
+      # Convert to RFC8345 topology data
+      # @return [Hash]
+      def topo_data
+        {
+          'prefix' => @prefix,
+          'next-hop' => @next_hop,
+          'interface' => @interface,
+          'metric' => @metric,
+          'preference' => @preference,
+          'description' => @description
+        }
+      end
+    end
+  end
+end
